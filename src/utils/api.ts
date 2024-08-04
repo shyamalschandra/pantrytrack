2bd2d41a6886c04d7b9a7045ad50db7f4db334e6
Shyamal Chandra <shyamalc@icloud.com>
2024-08-04 13:58:22 -0500

    Shyamal's last stand

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -7,17 +7,24 @@ export const fetchPantryItems = async () => {
   ]);
 };
 
-export const addPantryItem = async (item) => {
+// Add an interface definition for Item
+interface Item {
+  id: number;
+  name: string;
+  quantity: number;
+}
+
+export const addPantryItem = async (item: Item) => {
   // Simulated API call
   return Promise.resolve({ ...item, id: Date.now() });
 };
 
-export const updatePantryItem = async (item) => {
+export const updatePantryItem = async (item: Item) => {
   // Simulated API call
   return Promise.resolve(item);
 };
 
-export const deletePantryItem = async (id) => {
+export const deletePantryItem = async (_id: number) => {
   // Simulated API call
   return Promise.resolve();
 };
